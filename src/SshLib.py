9403e4ee58c80160a6a675f4ba6f92f30b9d4d1a
Preetham MS <me@preetham.in>
2015-03-24 18:43:42 -0400

    SSHConnection run function will return the command exit code
    Signed-off-by: Preetham MS <me@preetham.in>

--- src/SshLib.py
+++ src/SshLib.py
@@ -11,7 +11,7 @@ class SshConnection(object):
 		self.ssh.connect(host,username=self.user,password=self.password)
 
 
-	def run(self,command,password=None):
+	def run_old(self,command,password=None):
 		channel = self.ssh.get_transport().open_session()
 		if password==None:
 			get_pty=False
@@ -30,6 +30,27 @@ class SshConnection(object):
 			print line.strip()
 		#exit_code =channel.recv_exit_status()
 		#print"Exit Code=",exit_code
+		#if exit_code!=0:
+		#	exit()
+
+	def run(self,command,password=None):
+		#channel = self.ssh.get_transport().open_session()
+		if password==None:
+			get_pty=False
+		else:
+			get_pty=True
+		print "Running command: "+command
+		stdin,stdout,stderr = self.ssh.exec_command(command,get_pty=get_pty)
+		if password!=None:
+			stdin.write(password+"\n")
+			stdin.flush()
+		stdin.close()
+		for line in stdout.readlines():
+			print line.strip()
+		for line in stderr.readlines():
+			print line.strip()
+		exit_code =stdout.channel.recv_exit_status() 
+		return exit_code
 		#if exit_code!=0:
 		#	exit()
 
