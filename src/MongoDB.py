cadd793971e4a8f8eea1eb0dfb796fd636751371
Preetham MS <me@preetham.in>
2015-04-03 16:13:20 -0400

    Adding mongodb.py
    Signed-off-by: Preetham MS <me@preetham.in>

---
+++ src/MongoDB.py
@@ -0,0 +1,13 @@
+from SshLib import SshConnection
+
+def installMongoDB(con):
+	con.run("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10")
+	con.run('echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list')
+	con.run("sudo apt-get update")
+	con.run("sudo apt-get install -y mongodb-org")
+	con.run("sudo service mongod start")
+
+
+if __name__=="__main__":
+	con=SshConnection("152.1.13.208","pmahish",password="Bazinga23")
+	installMongoDB(con)
