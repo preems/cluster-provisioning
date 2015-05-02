7f23b266bc53a0aa47301a0bf8ee3b1347dae627
Preetham MS <me@preetham.in>
2015-05-01 21:30:19 -0400

    Refractoring code
    Signed-off-by: Preetham MS <me@preetham.in>

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from config import Configuration
+from DoLib import Droplet,initDO
+from SshLib import SshConnection
+from AWSLib import AWSInstance
+from HadoopLib import installHadoop
+from Mongo import installMongoDB
